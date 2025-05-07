e0123cee894eb73db02906651db1733855c478dd
Cedric Kienzler <github@cedric-kienzler.de>
2025-05-07 17:36:16 +0200

    cleanup commands

--- cmd/bladectl/cmd_identify.go
+++ cmd/bladectl/cmd_identify.go
@@ -15,19 +15,27 @@ var (
 )
 
 func init() {
-	cmdIdentify.Flags().BoolVarP(&confirm, "confirm", "c", false, "confirm the identify state")
-	cmdIdentify.Flags().BoolVarP(&wait, "wait", "w", false, "Wait for the identify state to be confirmed (e.g. by a physical button press)")
-	cmdSet.AddCommand(cmdIdentify)
+	cmdSetIdentify.Flags().BoolVarP(&confirm, "confirm", "c", false, "confirm the identify state")
+	cmdSetIdentify.Flags().BoolVarP(&wait, "wait", "w", false, "Wait for the identify state to be confirmed (e.g. by a physical button press)")
+	cmdSet.AddCommand(cmdSetIdentify)
+	cmdRemove.AddCommand(cmdRmIdentify)
 }
 
-var cmdIdentify = &cobra.Command{
+var cmdSetIdentify = &cobra.Command{
 	Use:     "identify",
 	Example: "bladectl set identify --wait",
 	Short:   "interact with the compute-blade identity LED",
-	RunE:    runIdentity,
+	RunE:    runSetIdentify,
 }
 
-func runIdentity(cmd *cobra.Command, _ []string) error {
+var cmdRmIdentify = &cobra.Command{
+	Use:     "identify",
+	Example: "bladectl unset identify",
+	Short:   "remove the identify state with the compute-blade identity LED",
+	RunE:    runRemoveIdentify,
+}
+
+func runSetIdentify(cmd *cobra.Command, _ []string) error {
 	var err error
 
 	ctx := cmd.Context()
@@ -57,6 +65,24 @@ func runIdentity(cmd *cobra.Command, _ []string) error {
 
 	if _, err := client.WaitForIdentifyConfirm(ctx, &emptypb.Empty{}); err != nil {
 		return humane.Wrap(err, "unable to wait for confirmation", "ensure the compute-blade agent is running and responsive to requests", "check the compute-blade agent logs for more information using 'journalctl -u compute-blade-agent.service'")
+	}
+
+	return nil
+}
+
+func runRemoveIdentify(cmd *cobra.Command, _ []string) error {
+	ctx := cmd.Context()
+	client := clientFromContext(ctx)
+
+	// Emit the event to the compute-blade-agent
+	_, err := client.EmitEvent(ctx, &bladeapiv1alpha1.EmitEventRequest{Event: bladeapiv1alpha1.Event_IDENTIFY_CONFIRM})
+	if err != nil {
+		return fmt.Errorf(humane.Wrap(err,
+			"failed to emit event",
+			"ensure the compute-blade agent is running and responsive to requests",
+			"check the compute-blade agent logs for more information using 'journalctl -u compute-blade-agent.service'",
+		).Display(),
+		)
 	}
 
 	return nil
